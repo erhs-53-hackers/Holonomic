063941ef3f5abfedf2fcddbafacc718b3591f33c
Michael Stevens <mls0.5mv2@gmail.com>
2011-11-20 15:53:38 -0500

    Ading circle logic

--- Holonomic/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ Holonomic/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -36,7 +36,7 @@ public class RobotTemplate extends IterativeRobot {
         msg = new Messager();
         //motor = new Jaguar(1);
         msg.printLn("Michael");
-        msg.printLn("11/16/11 Testing Holonomic Drive");
+        msg.printLn("11/19/11 Testing Holonomic Drive");
 
         getWatchdog().setExpiration(10);
         driveTrain.setSafetyEnabled(false);
@@ -46,13 +46,47 @@ public class RobotTemplate extends IterativeRobot {
      * This function is called periodically during autonomous
      */
     int stage = 0;
-    float speed = .5f;
+    float speed = 0.3f;
+    float inc = 0.05f;
+    float x = 1;
+    float y = 0;
+
+    private void circle() {
+        switch (stage) {
+            case 0:
+                x -= inc;
+                y += inc;
+                if(x == 0) stage++;
+                break;
+            case 1:
+                x -= inc;
+                y -= inc;
+                if(x == -1) stage++;
+                break;
+            case 2:
+                x += inc;
+                y -= inc;
+                if(x == 0) stage++;
+                break;
+            case 3:
+                x += inc;
+                y += inc;
+                if(x == 1) stage = 0;
+                break;
+        }
+
+        driveTrain.mecanumDrive_Cartesian(x * speed, y * speed, 0, 0);
+        Timer.delay(0.3);
+        String s = "stage:"+stage+"X:"+x+"Y:"+y;
+        msg.printLn(s);
+    }
 
     public void autonomousPeriodic() {
 
+        /*
         driveTrain.mecanumDrive_Cartesian(speed, 0, 0, 0);
         Timer.delay(1);
-        driveTrain.stopMotor();        
+        driveTrain.stopMotor();
         driveTrain.mecanumDrive_Cartesian(0, speed, 0, 0);
         Timer.delay(1);
         driveTrain.stopMotor();
@@ -62,25 +96,15 @@ public class RobotTemplate extends IterativeRobot {
         driveTrain.mecanumDrive_Cartesian(0, -speed, 0, 0);
         Timer.delay(1);
         driveTrain.stopMotor();
-        
+         * 
+         */
+        circle();
 
 
     }
 
     public void teleopPeriodic() {
         driveTrain.mecanumDrive_Cartesian(joystick.getX(), joystick.getY(), joystick.getZ(), 0);
-       // driveTrain.mecanumDrive_Cartesian(speed, speed, 0, 0);
+        // driveTrain.mecanumDrive_Cartesian(speed, speed, 0, 0);
     }
-
-
-
-   
-
-
-
-
-    
-
-    
-    
 }
